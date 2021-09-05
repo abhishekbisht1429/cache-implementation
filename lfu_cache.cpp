7a622e7da7f6081999b595a15d70b92899331a7a
Abhishek Bisht <abhishekbisht1429@gmail.com>
2021-09-06 00:18:09 +0530

    implemented lfu cache

--- lfu_cache.cpp
+++ lfu_cache.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<unordered_map>
+#include<chrono>
 
 using namespace std;
 
@@ -6,14 +8,23 @@ struct node {
     int key;
     int val;
     int freq;
+    int pos;
+    long long ts;
 
-    node(): key(0), val(0), freq(0) {}
+    node(): key(0), val(0), freq(0), pos(-1) {}
+
+    node(int key, int val, int freq):
+        key(key), val(val), freq(freq), pos(-1) {}
 
     bool operator<(const node &nd) {
-        return this->freq < nd.freq;
+        return (this->freq < nd.freq)?true:(this->ts < nd.ts); 
     }
 };
 
+long long get_time_stamp() {
+    return chrono::duration_cast<chrono::nanoseconds>(
+                chrono::system_clock::now().time_since_epoch()).count();
+}
 class heap {
     node **tree;
     size_t cap;
@@ -28,13 +39,18 @@ class heap {
             tree[i] = nullptr;
     }
 
+    void swap_nodes(node *&n1, node *&n2) {
+        swap(n1, n2);
+        swap(n1->pos, n2->pos);
+    }
+
     private:void heapifyUp(int node) {
         if(node == 0)
             return;
         
         int p = (node-1)/2;
         if(*tree[node] < *tree[p]) {
-            swap(tree[node], tree[p]);
+            swap_nodes(tree[node], tree[p]);
             heapifyUp(p);
         }
     }
@@ -58,10 +74,10 @@ class heap {
         }
 
         if(choice == 1) {
-            swap(tree[root], tree[l]);
+            swap_nodes(tree[root], tree[l]);
             heapifyDown(l);
         } else if(choice == 2) {
-            swap(tree[root], tree[r]);
+            swap_nodes(tree[root], tree[r]);
             heapifyDown(r);
         }
     }
@@ -71,6 +87,8 @@ class heap {
             return;
         
         tree[size] = nd;
+        tree[size]->pos = size;
+        tree[size]->ts = get_time_stamp();
         heapifyUp(size++);
     }
 
@@ -80,50 +98,122 @@ class heap {
         return tree[0];
     }
 
-    public:void pop() {
+    public:node* pop() {
         if(size == 0)
-            return;
+            return nullptr;
         
-        swap(tree[0], tree[size-1]);
+        swap_nodes(tree[0], tree[size-1]);
+        node *temp = tree[size-1];
         tree[--size] = nullptr;
         heapifyDown(0);
+        
+        return temp;
     }
 
     public:~heap() {
         delete[] tree;
     }
 
+    public:void update(node *nd, int val) {
+        nd->val = val;
+        refer(nd);
+    }
+
+    public:void refer(node *nd) {
+        nd->freq++;
+        nd->ts = get_time_stamp();
+        heapifyDown(nd->pos);
+        heapifyUp(nd->pos);
+    }
+
     public:void print() {
         for(int i=0; i<size; ++i)
-            cout<<tree[i]->freq<<" ";
+            cout<<tree[i]->key<<" ";
+        cout<<"\n";
+    }
+};
+
+class lfu_cache {
+    unordered_map<int, node*> cache;
+    heap *min_heap;
+
+    int cap;
+    int size;
+
+    public:lfu_cache(int cap): cap(cap), size(0) {
+        min_heap = new heap(cap);
+    }
+
+    public:~lfu_cache() {
+        delete min_heap;
+    }
+
+    public:void set(int key, int val) {
+        if(cache.find(key) != cache.end()) {
+            min_heap->update(cache[key], val);
+        } else {
+            if(size == cap) {
+                node *temp = min_heap->pop();
+                cache.erase(temp->key);
+                size--;
+                delete temp;
+            }
+            cache[key] = new node(key, val, 1);
+            min_heap->insert(cache[key]);
+            size++;
+        }
+    }
+
+    public:int get(int key) {
+        if(cache.find(key) == cache.end())
+            return -1;
+        min_heap->refer(cache[key]);
+        return cache[key]->val;
+    }
+
+    public:void display() {
+        for(auto &p : cache) {
+            cout<<p.second->val<<" ";
+        }
         cout<<"\n";
     }
 };
 
 int main() {
-    node n1, n2, n3, n4, n5, n6, n7;
-    n1.freq = 10;
-    n2.freq = 2;
-    n3.freq = 3;
-    n4.freq = 4;
-    n5.freq = 34;
-    n6.freq = 0;
-    n7.freq = -1;
+    lfu_cache cache(3);
+    int arr[] = {1, 2, 3, 4, 1, 2, 5, 1, 2, 3, 4, 5};
 
-    heap h(8);
-    h.insert(&n1);
-    h.insert(&n2);
-    h.insert(&n3);
-    h.insert(&n4);
+    for(int i=0; i<sizeof(arr)/sizeof(int); ++i) {
+        cache.set(arr[i], arr[i]*1000);
+        cout<<arr[i]<<" ";
+        cache.display();
+    }
+    // node n1, n2, n3, n4, n5, n6, n7;
+    // n1.freq = 10;
+    // n2.freq = 2;
+    // n3.freq = 3;
+    // n4.freq = 4;
+    // n5.freq = 34;
+    // n6.freq = 0;
+    // n7.freq = -1;
 
-    h.print();
-    h.pop();
-    h.print();
+    // heap h(6);
+    // h.insert(&n1);
+    // h.insert(&n2);
+    // h.insert(&n3);
+    // h.insert(&n4);
 
-    h.insert(&n5);
-    h.insert(&n6);
-    h.insert(&n7);
-    h.pop();
+    // h.print();
+    // h.pop();
+    // h.print();
 
-    h.print();
+    // h.insert(&n5);
+    // h.insert(&n6);
+    // h.pop();
+    // h.insert(&n7);
+
+    // h.print();
+    
+    // h.refer(&n7);
+    // h.print();
 }
